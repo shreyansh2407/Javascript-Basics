fc2d7571ec7f8da55ff79213b58e3cf242cc6c09
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-13 16:08:44 +0530

    First program

---
+++ index.js
@@ -0,0 +1,33 @@
+//Line below is use to print statement on console windows
+console.log("Shreyansh");
+console.log("Web developer");
+
+
+// let keyword is used for a code scope while var keyword is used when we required to access variabbe from anywhere
+
+let a=5; //integer variable
+console.log(a);
+
+let b = "Shreyansh";  //string variable
+console.log(b);
+
+let c = true; // boolean variable
+console.log(c);
+
+let d = 45.6; // float variable
+console.log(d);
+
+d="Sneha";
+console.log(d);
+
+const name = "Shreyansh";
+
+var pi = 3.14;
+console.log(pi);
+
+var pi = 4.56;
+console.log(pi);
+
+let age=17;
+let status= (age>=18)? "Yes vote" : "No vote";  //ternary operator 
+console.log(status);
