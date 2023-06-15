80c217df255e2d634ed6d40d5837c4c68dd70e05
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-15 11:39:12 +0530

    For of loop

---
+++ forOf.js
@@ -0,0 +1,12 @@
+let rectangle = {
+    length:1,
+    breadth:2.
+};
+
+for(let key of Object.keys(rectangle)){
+    console.log(key);
+}
+for(let key of Object.entries(rectangle)){
+    console.log(key);
+}
+
