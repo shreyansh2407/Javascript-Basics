0214de87f9c7d0f7a10df87e3593b0e01b80b6ab
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-18 10:50:09 +0530

    Methods

---
+++ methods.js
@@ -0,0 +1,12 @@
+let a  = Math.random() //It gives you random number between 0 and 1
+console.log(a)
+
+let lastname =  "This is Shreyansh singh";
+let words = lastname.split(' '); // It splits the sentences on the basis of parameter passed in split methods
+console.log(words);
+
+console.log(lastname.length);
+let upper = lastname.toUpperCase();
+console.log(upper);
+let trim = lastname.trimEnd();
+console.log(trim);
