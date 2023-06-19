efa15b7b8e31eda8b0f5503a8d6dcb149dcbef4a
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-19 19:44:32 +0530

    Template literal

---
+++ templateLiteral.js
@@ -0,0 +1,9 @@
+//template literal
+
+let message = `This is  
+shreyansh 
+singh 
+a 
+programmer
+"software developer"`;
+console.log(message);  //backticks helps you in printing the string as it is
