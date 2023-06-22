7f84c8991027c553a113e76be30b260142460821
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-22 11:32:07 +0530

    Array slicing

---
+++ arraySlicing.js
@@ -0,0 +1,6 @@
+let numbers = [10,20,30,40,50,60,70,80,90];
+let ans = numbers.slice(3,8);//starts from 3rd index and ends at 8-1 index
+console.log(ans);
+
+let copy = numbers.slice();//Copy all the elements from the number array
+console.log(copy);
