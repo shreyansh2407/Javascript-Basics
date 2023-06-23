3ce9bcd8e7e151fa9935347f1368a8c4d07db85a
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-24 02:21:03 +0530

    Array removing

---
+++ arrayRemoving.js
@@ -0,0 +1,9 @@
+//Removing elements from array
+
+let numbers = [1,2,3,4,5,6,7,8];
+numbers.pop();//remove elements from the last
+console.log(numbers);
+numbers.shift();//remove elements from the beginning
+console.log(numbers);
+numbers.splice(3,1);//remove elements from the desired index
+console.log(numbers);
