4d1e9fdec25c663bb7843430b765d1a4b78884d9
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-23 21:32:07 +0530

    Array insertion

---
+++ arrayInsertion.js
@@ -0,0 +1,9 @@
+//Insertion in arrays
+
+let arr = [1,4,5,9];
+arr.push(9); //Inserting elements in last
+console.log(arr);
+arr.unshift(7);//Inserting elements in beginning
+console.log(arr);
+arr.splice(3,0,'S','S');//Inserting elements on given index
+console.log(arr);
